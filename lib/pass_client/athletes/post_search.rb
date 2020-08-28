4aa6dba34b028bed63db481823706c5d52648ab4
Timothy <timothyfbowen@users.noreply.github.com>
2020-08-28 11:26:22 -0400

    TCT-1812 (#10)
    * TCT-1812 add option to search via POST request
    
    * TCT-1812 add search documentation
    
    * Bump version

---
+++ lib/pass_client/athletes/post_search.rb
@@ -0,0 +1,25 @@
+require 'pass_client/athletes/shared'
+
+module PassClient
+  module Athlete
+    class PostSearch
+      include Athlete
+
+      attr_reader :search_terms
+
+      def initialize(search_terms:)
+        @search_terms = search_terms
+      end
+
+      def post
+        execute
+      end
+
+      private
+
+      def connect
+        connection.post(url: "/api/partner_athlete_search/v1/search/athlete", body: search_terms.to_json, headers: auth_header)
+      end
+    end
+  end
+end
