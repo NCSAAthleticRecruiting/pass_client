d391cd3227dc805eabcbd72f8bd56f96cb3e6952
Timothy F. Bowen <tim@berecruited.com>
2021-01-06 13:02:41 -0500

    TCT-2041 add test

---
+++ spec/lib/pass_client/athletes/hard_deleter_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "pass_client/athletes/hard_deleter"
+
+RSpec.describe PassClient::Athlete::HardDeleter do
+  subject { described_class.new(id: id) }
+
+  let(:token_manager_double) { instance_double(PassClient::TokenManager) }
+  let(:connection_double) { instance_double(PassClient::Connection) }
+  let(:id) { "123-abc-456" }
+  let(:api_response) { Faraday::Response.new(status: 200, body: "") }
+  let(:method) { :delete }
+  let(:token) { "atoken" }
+
+  before do
+    allow(PassClient::TokenManager).to receive(:new).and_return(token_manager_double)
+    allow(token_manager_double).to receive(:renew!).and_return(token)
+    allow(token_manager_double).to receive(:token!).and_return(token)
+    allow(PassClient::Connection)
+      .to receive(:unsigned_instance)
+      .and_return(connection_double)
+    allow(connection_double)
+      .to receive(method)
+      .and_return(api_response)
+  end
+
+  it "sends a request to the correct address" do
+    expect(connection_double)
+      .to receive(method).with(url: "/api/partner_athlete_search/v1/athlete/#{id}?hard_delete=true", headers: { authorization: token })
+    subject.delete!
+  end
+
+  it "renew the jwt ONCE when the status == 401" do
+    ::PassClient.configuration.token = token
+    api_response = Faraday::Response.new(status: 401, body: "Error")
+    expect(connection_double)
+      .to receive(method)
+      .and_return(api_response)
+      .exactly(2).times
+
+    allow(subject).to receive(:token).and_return(token)
+    expect { subject.delete! }.to_not raise_error
+  end
+end
